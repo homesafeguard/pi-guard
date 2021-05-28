3a8a9eb527d814749934775e0c4a53868097d2da
Jonathan SAHM <pro@jsahm.fr>
2021-05-28 21:56:16 +0200

    update

--- src/opt/piguard/daemon.sh
+++ src/opt/piguard/daemon.sh
@@ -3,22 +3,43 @@ set -euo pipefail
 
 . "${PI_GUARD_OPT_DIR}/lib/helpers.sh"
 
-print_log "daemon" "INFO" "Start Pi-guard"
+daemonStartFunc () {
+  print_log "daemon" "INFO" "Start Pi-guard"
 
-# Waiting network
-piguard network --is-up
+  # Waiting network
+  piguard network --is-up
 
-# Waiting DNS server
-piguard dns --is-up
+  # Waiting DNS server
+  piguard dns --is-up
 
-# Watch network uptime
-piguard network --uptime &
+  # Watch network uptime
+  piguard network --uptime &
 
-# Watch DNS server uptime
-piguard dns --uptime &
+  # Watch DNS server uptime
+  piguard dns --uptime &
 
-# Fetch lists
-piguard fetch
+  # Fetch lists
+  piguard fetch
 
-# Reload lists
-piguard reload
+  # Reload lists
+  piguard reload
+}
+
+daemonStartFunc () {
+  print_log "daemon" "INFO" "Stop Pi-guard"
+}
+
+helpFunc() {
+  echo "Usage: piguard daemon
+Pi-guard daemon
+  -h, --help          Show this help dialog
+  --start             Start Pi-guard daemon
+  --stop              Stop Pi-guard daemon";
+  exit 0
+}
+
+case "${1:-}" in
+  "--start"            ) daemonStartFunc "$@";;
+  "--stop"             ) daemonStopFunc "$@";;
+  *                    ) helpFunc "$@";;
+esac

da0669be9d2039f45ab06b8369caa9ba33d9e1a3
Jonathan SAHM <pro@jsahm.fr>
2021-05-28 08:23:52 +0200

    Update

--- src/opt/piguard/service.sh
+++ src/opt/piguard/service.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+. "${PI_GUARD_OPT_DIR}/lib/helpers.sh"
+
 print_log "uptime" "INFO" "Start Pi-guard"
 
+# Waiting network
 piguard network --is-up
+
+# Waiting DNS server
 piguard dns --is-up
 
+# Watch network uptime
 piguard network --uptime &
+
+# Watch DNS server uptime
 piguard dns --uptime &
 
+# Fetch lists
 piguard fetch
+
+# Reload lists
 piguard reload

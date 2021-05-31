83b3d6bddc47a519cd6e22cb25e5a9f65dc2cb3c
Jonathan SAHM <pro@jsahm.fr>
2021-05-31 08:45:34 +0200

    Update

--- src/opt/piguard/selfupdate.sh
+++ src/opt/piguard/selfupdate.sh
@@ -23,7 +23,7 @@ updateFilesFunc() {
 
   ${PI_GUARD_SUDO} cp -frT "${PI_GUARD_GIT_DIR}/src" /
   ${PI_GUARD_SUDO} chown -R pi:pi "${PI_GUARD_ETC_DIR}"
-  ${PI_GUARD_SUDO} chown -R pi:pi "${PI_GUARD_LOG_FILE}"
+  ${PI_GUARD_SUDO} chown -R pi:pi "${PI_GUARD_LOG_DIR}"
 
   print_textnl "$(cd "${PI_GUARD_GIT_DIR}/src" && find . -type f ! -name ".gitignore" | sed 's/^\.\(.*\)$/ - \1/g')"
   print_log "self-update" "INFO" "${message}"

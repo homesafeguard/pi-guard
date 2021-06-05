073f5f83ae668bbd56dc09b79903a14f27ef73a3
Jonathan SAHM <pro@jsahm.fr>
2021-06-05 18:22:17 +0200

    Update

--- src/opt/piguard/daemon.sh
+++ src/opt/piguard/daemon.sh
@@ -3,28 +3,20 @@ set -euo pipefail
 
 . "${PI_GUARD_OPT_DIR}/lib/helpers.sh"
 
-daemonStartPre() {
-  print_log "daemon" "INFO" "Pre start Pi-guard"
+daemonStart() {
+  print_log "daemon" "INFO" "Start Pi-guard"
 
   # Waiting network
   piguard network --is-up
 
   # Waiting DNS server
   piguard dns --is-up
-}
-
-daemonStart() {
-  print_log "daemon" "INFO" "Start Pi-guard"
 
   # Watch network uptime
   piguard network --uptime &
 
   # Watch DNS server uptime
   piguard dns --uptime &
-}
-
-daemonStartPost() {
-  print_log "daemon" "INFO" "Post start Pi-guard"
 
   # Fetch lists
   piguard fetch
@@ -41,17 +33,13 @@ help() {
   echo "Usage: piguard daemon
 Pi-guard daemon
   -h, --help          Show this help dialog
-  --start-pre         Pre Start Pi-guard daemon
   --start             Start Pi-guard daemon
-  --start-post        Post start Pi-guard daemon
   --stop              Stop Pi-guard daemon";
   exit 0
 }
 
 case "${1:-}" in
-  "--start-pre"        ) daemonStartPre "$@";;
   "--start"            ) daemonStart "$@";;
-  "--start-post"       ) daemonStartPost "$@";;
   "--stop"             ) daemonStop "$@";;
   *                    ) help "$@";;
 esac

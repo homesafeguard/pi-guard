2a739bdab2b9a242decb835cb0740736355a0b51
Jonathan SAHM <pro@jsahm.fr>
2021-06-12 21:25:40 +0200

    Updat

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Safe network for home
 
 ## Installation
 
-    sh -c "$(wget https://raw.githubusercontent.com/homesafeguard/pi-guard/main/install.sh -O -)"
+    sudo sh -c "$(wget https://raw.githubusercontent.com/homesafeguard/pi-guard/main/install.sh -O -)"
--- install.sh
+++ install.sh
@@ -1,55 +1,64 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PI_GUARD_SUDO="$(sh -c "if [ 0 != \"${EUID}\" ]; then echo 'sudo'; fi")"
-
-## Remove bluetooth
-${PI_GUARD_SUDO} systemctl disable hciuart.service
-${PI_GUARD_SUDO} systemctl disable bluealsa.service
-${PI_GUARD_SUDO} systemctl disable bluetooth.service
-${PI_GUARD_SUDO} apt remove -y --purge bluez
-
-## Remove Wifi
-${PI_GUARD_SUDO} systemctl disable wpa_supplicant.service
+if [ 0 != "${EUID}" ]; then
+  echo 'I must be run by root'
+  exit
+fi
 
 ## Remove dhcpcd
-${PI_GUARD_SUDO} apt remove -y --purge dhcpcd5
+apt remove -y --purge dhcpcd5
+
+## Disable bluetooth
+systemctl disable hciuart.service
+systemctl disable bluealsa.service
+systemctl disable bluetooth.service
+apt remove -y --purge bluez
+
+## Disable Wifi
+systemctl disable wpa_supplicant.service
 
 ## Install dependencies
-${PI_GUARD_SUDO} apt update
-${PI_GUARD_SUDO} apt dist-upgrade
-${PI_GUARD_SUDO} apt install -y curl git dnsutils dnsmasq iperf
+apt update
+apt dist-upgrade
+apt install -y curl git dnsutils dnsmasq
 
 ## Install piguard repository
 if [[ ! -d "/etc/.piguard" ]]; then
-  ${PI_GUARD_SUDO} git clone https://github.com/homesafeguard/pi-guard.git /etc/.piguard
+  git clone https://github.com/homesafeguard/pi-guard.git /etc/.piguard
 fi
 
 ## Install dnscrypt
 if [[ ! -d "/opt/dnscrypt-proxy" ]]; then
   cd /opt || exit
-  ${PI_GUARD_SUDO} wget https://github.com/DNSCrypt/dnscrypt-proxy/releases/download/2.0.45/dnscrypt-proxy-linux_arm-2.0.45.tar.gz -O /opt/dnscrypt-proxy.tar.gz
-  ${PI_GUARD_SUDO} tar -xf dnscrypt-proxy.tar.gz
-  ${PI_GUARD_SUDO} rm /opt/dnscrypt-proxy.tar.gz
-  ${PI_GUARD_SUDO} mv linux-arm dnscrypt-proxy
+  wget https://github.com/DNSCrypt/dnscrypt-proxy/releases/download/2.0.45/dnscrypt-proxy-linux_arm-2.0.45.tar.gz -O /opt/dnscrypt-proxy.tar.gz
+  tar -xf dnscrypt-proxy.tar.gz
+  rm /opt/dnscrypt-proxy.tar.gz
+  mv linux-arm dnscrypt-proxy
   cd dnscrypt-proxy || exit
 fi
 
-## Copy files
-${PI_GUARD_SUDO} cp -frT /etc/.piguard/src /
+## Copy piguard files
+cp -frT /etc/.piguard/src /
 
 ## Start dnscrypt
-${PI_GUARD_SUDO} /opt/dnscrypt-proxy/dnscrypt-proxy -service install
-${PI_GUARD_SUDO} /opt/dnscrypt-proxy/dnscrypt-proxy -service start
+/opt/dnscrypt-proxy/dnscrypt-proxy -service install
+/opt/dnscrypt-proxy/dnscrypt-proxy -service start
 
-## Start dnsmasq
-${PI_GUARD_SUDO} systemctl restart dnsmasq
+## Restart dnsmasq service
+systemctl restart dnsmasq
 
-## Enable service
-${PI_GUARD_SUDO} systemctl enable piguard.service
+## Restart syslog service
+systemctl restart rsyslog
 
-## Restart daemon
-${PI_GUARD_SUDO} systemctl daemon-reload
+## Enable SSH service
+systemctl enable ssh
 
-## Restart syslog
-${PI_GUARD_SUDO} systemctl restart rsyslog
+## Enable piguard service
+systemctl enable piguard.service
+
+## Reload daemon
+systemctl daemon-reload
+
+## Reboot
+reboot

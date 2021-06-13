8a6c8353a748cdfdad92424c13b1cc1e5bb9ead3
Jonathan SAHM <pro@jsahm.fr>
2021-06-13 10:01:35 +0200

    Update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -eu
 
 if [ "$(id -u)" -ne 0 ]; then
@@ -6,55 +6,54 @@ if [ "$(id -u)" -ne 0 ]; then
   exit
 fi
 
+readonly DNSCRYPT_PATH='/opt/dnscrypt-proxy'
+readonly PIGUARD_PATH='/etc/.piguard'
+
 ## Remove dhcpcd
 apt remove -y --purge dhcpcd5
 
 ## Disable bluetooth
-if service --status-all | grep -Fq 'hciuart'; then
+if service --status-all 2>/dev/null | grep -Fq 'hciuart'; then
   systemctl disable hciuart.service
 fi
-if service --status-all | grep -Fq 'bluealsa'; then
+if service --status-all 2>/dev/null | grep -Fq 'bluealsa'; then
   systemctl disable bluealsa.service
 fi
-if service --status-all | grep -Fq 'bluetooth'; then
+if service --status-all 2>/dev/null | grep -Fq 'bluetooth'; then
   systemctl disable bluetooth.service
 fi
 apt remove -y --purge bluez
 
 ## Disable Wifi
-if service --status-all | grep -Fq 'wpa_supplicant'; then
+if service --status-all 2>/dev/null | grep -Fq 'wpa_supplicant'; then
   systemctl disable wpa_supplicant.service
 fi
 
 ## Install dependencies
 apt update
 apt dist-upgrade -y
-apt install -y curl git dnsutils dnsmasq
+apt install -y curl git dnsutils dnsmasq iptables ipset
 apt autoremove -y --purge
 apt autoclean
 
 ## Install piguard repository
-if [ ! -d "/etc/.piguard" ]; then
-  git clone https://github.com/homesafeguard/pi-guard.git /etc/.piguard
+if [ ! -d "${PIGUARD_PATH}" ]; then
+  git clone https://github.com/homesafeguard/pi-guard.git "${PIGUARD_PATH}"
 fi
 
 ## Install dnscrypt
-if [ ! -d "/opt/dnscrypt-proxy" ]; then
+if [ ! -d "${DNSCRYPT_PATH}" ]; then
   cd /opt || exit
   wget https://github.com/DNSCrypt/dnscrypt-proxy/releases/download/2.0.45/dnscrypt-proxy-linux_arm-2.0.45.tar.gz -O /opt/dnscrypt-proxy.tar.gz
   tar -xf dnscrypt-proxy.tar.gz
   rm /opt/dnscrypt-proxy.tar.gz
   mv linux-arm dnscrypt-proxy
-  cd dnscrypt-proxy || exit
+  cd "${DNSCRYPT_PATH}" || exit
+  "${DNSCRYPT_PATH}/dnscrypt-proxy" -service install
 fi
 
 ## Copy piguard files
-cp -frT /etc/.piguard/src /
-
-## Install dnscrypt
-if ! service --status-all | grep -Fq 'dnscrypt'; then
-  /opt/dnscrypt-proxy/dnscrypt-proxy -service install
-fi
+cp -frT "${PIGUARD_PATH}/src" /
 
 ## Restart dnsmasq service
 systemctl restart dnsmasq
